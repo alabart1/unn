9b5c3eceb080f5af4e16d06aa56bb156d7a2d149
alvar <jsoalvar@gmail.com>
2020-09-19 18:02:44 -0400

    incorporado

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost/sistema',
+mongoose.connect('mongodb://mongodb+srv://admin:admin@cluster0.gc3l7.mongodb.net/test',
 {
     useCreateIndex:true,
     useNewUrlParser:true,
